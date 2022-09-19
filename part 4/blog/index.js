32a81b6d069c65857157e40a4f3c2496c28500c7
skarussel <steven.kocadag@st.ovgu.de>
2022-09-19 13:58:26 +0300

    4.2

--- part 4/blog/index.js
+++ part 4/blog/index.js
@@ -1,39 +1,10 @@
+const app = require('./app') // the actual Express application
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const Blog = require('./models/blog')
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-
-app.get("", (request, response) => {
-    response.send("Hallo")
-})
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  
-  const blog = new Blog(request.body)
-  console.log(blog)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
