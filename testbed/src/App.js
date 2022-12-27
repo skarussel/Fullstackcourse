c5cfe73a0609fc44a340405f5c9fee621744a65e
Steven <steven.kocadag@st.ovgu.de>
2022-12-27 16:36:09 +0100

    init

---
+++ testbed/src/App.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react"
+const App = () => {
+    let x = 1
+    const y = 1
+    const t = []
+    t.push(1)
+    console.log(t)
+
+
+  
+    return (<div>
+       Hello
+      </div>)
+    }
+
+export default App
